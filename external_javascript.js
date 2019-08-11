f6e5d444f89dcf4021d17237e9ff2a103612481d
DJ-DON-GENIUS <53823189+DJ-DON-GENIUS@users.noreply.github.com>
2019-08-11 15:17:07 +0100

    Update external_javascript.js

--- external_javascript.js
+++ external_javascript.js
@@ -1,4 +1,4 @@
-
+<script>
 	window.onload=function(){
 	var audioT=document.getElementById("audioD");
 	var audioMix =["hiphop_mix.mp3","NAIJAMIX-36.mp3","testing (32).mp3","testing (36).mp3","CLUB_MIX_NIGERIA-26.mp3"];
@@ -17,3 +17,4 @@
 
 		}
 		}
+</script>
