c4f45fd934e641e8ee0882f4434d7a64eedd79d2
DJ-DON-GENIUS <53823189+DJ-DON-GENIUS@users.noreply.github.com>
2019-08-11 15:15:19 +0100

    Add files via upload

---
+++ external_javascript.js
@@ -0,0 +1,19 @@
+
+	window.onload=function(){
+	var audioT=document.getElementById("audioD");
+	var audioMix =["hiphop_mix.mp3","NAIJAMIX-36.mp3","testing (32).mp3","testing (36).mp3","CLUB_MIX_NIGERIA-26.mp3"];
+
+	for(i=0; i< audioMix.length; i++){
+	var audioE = document.createElement('audio');
+	var audioTitle=document.createElement('span');
+		audioTitle.textContent=audioMix[i];
+		audioE.src=audioMix[i];
+		audioE.controls= true;
+		audioE.autoplay= false;
+		audioT.appendChild(audioE);
+		audioT.appendChild(audioTitle);
+	var lin   =document.createElement('br');
+		audioT.appendChild(lin);
+
+		}
+		}
