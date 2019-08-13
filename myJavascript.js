1b62a459c02697ee2ddae138dcfa9f901c0c62ac
DJ-DON-GENIUS <53823189+DJ-DON-GENIUS@users.noreply.github.com>
2019-08-13 12:57:02 +0100

    Update myJavascript.js

--- myJavascript.js
+++ myJavascript.js
@@ -5,7 +5,6 @@
 
 	for(i=0; i< audioMix.length; i++){
 	var audioE = document.createElement('audio');
-	var lin   =document.createElement('br');
 	var audioTitle=document.createElement('a');
 		audioTitle.setAttribute("href", "downloads");
 		audioTitle.textContent=audioMix[i];
@@ -13,8 +12,10 @@
 		audioE.controls= true;
 		audioE.autoplay= false;
 		audioT.appendChild(audioE);
-		audioT.appendChild(lin);
 		audioT.appendChild(audioTitle);
+	var lin   =document.createElement('br');
+		audioT.appendChild(lin);
+		
 	
 		
 
