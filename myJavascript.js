907c8fbd0bb82826007606a9c68b4abfc13add16
DJ-DON-GENIUS <53823189+DJ-DON-GENIUS@users.noreply.github.com>
2019-08-14 19:31:16 +0100

    Update myJavascript.js

--- myJavascript.js
+++ myJavascript.js
@@ -4,8 +4,8 @@
 	var audioMix =["hiphop_mix.mp3","NAIJAMIX-36.mp3","testing (32).mp3","testing (36).mp3","CLUB_MIX_NIGERIA-26.mp3"];
 
 	for(i=0; i< audioMix.length; i++){
-	var audioE = document.createElement('audio')[i];
-	var audioTitle=document.createElement('a')[i];
+	var audioE = document.createElement('audio');
+	var audioTitle=document.createElement('a');
 		audioTitle.setAttribute("href", "postDownload");
 		audioTitle.setAttribute("onclick", "postPage(event)");
 		audioTitle.text=audioMix[i];
