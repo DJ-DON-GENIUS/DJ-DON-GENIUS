a84ad2012bb8f207599e5b71b12cfe7ada589551
DJ-DON-GENIUS <53823189+DJ-DON-GENIUS@users.noreply.github.com>
2019-08-23 23:55:29 +0100

    Update saveJson.php

--- saveJson.php
+++ saveJson.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submit'])) {
-	$file = "data.json";
+	$file = "https://raw.githubusercontent.com/DJ-DON-GENIUS/DJ-DON-GENIUS/master/data.json";
 	$json_string = json_encode($_POST, JSON_PRETTY_PRINT);
 	file_put_contents($file, $json_string, FILE_APPEND);
 	header('Location: thanks.php'); 
